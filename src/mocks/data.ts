5df55828d0d73855d347d20d39a5d3899faa9c24
oliver-distributed <tembokoliver3@gmail.com>
2022-03-21 23:57:50 +0300

    update

---
+++ src/mocks/data.ts
@@ -0,0 +1,11 @@
+import { Iinterpolate, IMatric2D } from "../types";
+
+export const data2D: IMatric2D  = {
+  matrix:[
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+  ]
+};
